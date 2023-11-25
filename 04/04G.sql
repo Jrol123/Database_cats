73c0e4600012c731e2d976518c08a3a99b1f4f24
Artemii <angap4@gmail.com>
2023-11-25 15:58:21 +1000

    Update 04G.sql
    V3

--- 04/04G.sql
+++ 04/04G.sql
@@ -2,15 +2,15 @@ SELECT date        AS date,
        count_true  AS number_of_new,
        count_false AS number_of_completed
 FROM (SELECT MAX(ID)                               AS ID,
-                     task_id,
-                     substr(timestamp, 0, 10 + 1)          as date,
+             task_id,
+             substr(timestamp, 0, 10 + 1)          as date,
 --        COUNt(status_type) as total_count,
-                     COUNT(IIF(status_type == 1, 1, NULL)) as count_true,
-                     COUNT(IIF(status_type == 2, 1, NULL)) as count_false
-              FROM incident_table
-              WHERE status_type in (1, 2)
-              GROUP BY substr(timestamp, 0, 10 + 1)
-              ORDER BY timestamp) as max_id_filter
+             COUNT(IIF(status_type == 1, 1, NULL)) as count_true,
+             COUNT(IIF(status_type == 2, 1, NULL)) as count_false
+      FROM incident_table
+      WHERE status_type in (1, 2)
+      GROUP BY substr(timestamp, 0, 10 + 1)
+      ORDER BY timestamp) as max_id_filter
 GROUP BY date
 ORDER BY date;
 
@@ -23,4 +23,15 @@ SELECT MAX(ID)                               AS ID,
 FROM incident_table
 WHERE status_type in (1, 2)
 GROUP BY substr(timestamp, 0, 10 + 1)
-ORDER BY timestamp;
+ORDER BY substr(timestamp, 0, 10 + 1);
+
+SELECT MAX(ID)                               AS ID,
+       task_id,
+       DATE(timestamp)                       as date,
+       COUNT(IIF(status_type == 1, 1, NULL)) AS number_of_new,
+       COUNT(IIF(status_type == 2, 1, NULL)) AS number_of_completed
+FROM incident_table
+WHERE status_type in (1, 2)
+  AND DATE(timestamp) >= DATE('now', '-30 day')
+GROUP BY DATE(timestamp)
+ORDER BY DATE(timestamp);
