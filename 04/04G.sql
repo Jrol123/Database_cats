f756b818a7dc11774fc036d59d2fbc5ac083afef
Artemii <angap4@gmail.com>
2023-11-25 12:00:40 +1000

    04G made filtration by (1, 2)

--- 04/04G.sql
+++ 04/04G.sql
@@ -1,10 +1,13 @@
-SELECT substr(max_id_filter.timestamp, 0, 10 + 1)    as date,
-       COUNT(max_id_filter.status_type == 1) as number_of_new,
-       COUNT(max_id_filter.status_type == 2) as number_of_completed
+SELECT substr(max_id_filter.date, 0, 10 + 1) as date
+--        SUM(CASE WHEN max_id_filter) as number_of_new
+--        COUNT(max_id_filter.status_type == 2) as number_of_completed
 FROM incident_table
-         JOIN(SELECT MAX(ID) AS ID, task_id, timestamp as date, status_type
+         JOIN(SELECT MAX(ID)   AS ID,
+                     task_id,
+                     timestamp as date,
+                     status_type as status
               FROM incident_table
-              GROUP BY timestamp, task_id
-              HAVING status_type in (1, 2)) AS max_id_filter
-GROUP BY max_id_filter.timestamp, max_id_filter.task_id
-ORDER BY max_id_filter.timestamp;
+              WHERE status_type in (1, 2)
+              GROUP BY timestamp) AS max_id_filter
+GROUP BY max_id_filter.date, max_id_filter.task_id
+ORDER BY max_id_filter.date;
