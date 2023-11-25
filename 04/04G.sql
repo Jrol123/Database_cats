b00ce5795f4662e1bf6b2a65260257666b0a114c
Artemii <angap4@gmail.com>
2023-11-25 12:17:21 +1000

    04G WA

--- 04/04G.sql
+++ 04/04G.sql
@@ -1,22 +1,24 @@
-SELECT substr(max_id_filter.date, 0, 10 + 1)                      AS date,
---        SUM(CASE WHEN max_id_filter) as number_of_new
-       SUM(CASE WHEN max_id_filter.status == 1 THEN 1 ELSE 0 END) AS number_of_completed
+SELECT date        AS date,
+       count_true  AS number_of_new,
+       count_false AS number_of_completed
 FROM incident_table
-         JOIN(SELECT MAX(ID)     AS ID,
+         JOIN(SELECT MAX(ID)                               AS ID,
                      task_id,
-                     timestamp   as date,
-                     status_type as status
+                     substr(timestamp, 0, 10 + 1)          as date,
+--        COUNt(status_type) as total_count,
+                     COUNT(IIF(status_type == 1, 1, NULL)) as count_true,
+                     COUNT(IIF(status_type == 2, 1, NULL)) as count_false
               FROM incident_table
               WHERE status_type in (1, 2)
-              GROUP BY timestamp
-              ORDER BY timestamp) AS max_id_filter
-GROUP BY substr(max_id_filter.date, 0, 10 + 1)
-ORDER BY max_id_filter.date;
+              GROUP BY substr(timestamp, 0, 10 + 1)
+              ORDER BY timestamp) as max_id_filter
+GROUP BY date
+ORDER BY date;
 
 SELECT MAX(ID)                               AS ID,
        task_id,
        substr(timestamp, 0, 10 + 1)          as date,
---        COUNt(status_type) as total_count,
+       COUNT(status_type)                    as total_count,
        COUNT(IIF(status_type == 1, 1, NULL)) as count_true,
        COUNT(IIF(status_type == 2, 1, NULL)) as count_false
 FROM incident_table
