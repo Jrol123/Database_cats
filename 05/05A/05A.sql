11d8b3ff90bff530c566a1f6ab1e07fedbbf27bb
Artemii <angap4@gmail.com>
2023-12-28 17:20:26 +1000

    05A AW

--- 05/05A/05A.sql
+++ 05/05A/05A.sql
@@ -1,13 +1,15 @@
--- Требуется подсчитать количество постояльцев (врачей и пациентов) больницы, у которых есть актуальные полисы ОМС.
--- (TotalPeopleWithMI), где TotalPeopleWithMI - количество постояльцев больницы с актуальными полисами ОМС.
-WITH ActiveInsurance AS (SELECT DISTINCT PersonalData.ID
-                         FROM MedicalInsurance
-                                  JOIN PersonalData ON MedicalInsurance.ID = PersonalData.MedicalInsuranceID
-                         WHERE Status = 'active')
+WITH ActiveInsurance AS (
+    -- Выбор людей с активной мед-страховкой
+    SELECT DISTINCT PersonalData.ID
+    FROM MedicalInsurance
+             JOIN PersonalData ON MedicalInsurance.ID = PersonalData.MedicalInsuranceID
+    WHERE Status = 'active')
 SELECT COUNT(*) AS TotalPeopleWithMI
-FROM (SELECT ActiveInsurance.ID
-      FROM ActiveInsurance
-               LEFT JOIN Doctors ON ActiveInsurance.ID = Doctors.PersonalDataID
-               LEFT JOIN Patients ON ActiveInsurance.ID = Patients.PersonalDataID
-      WHERE Doctors.ID IS NOT NULL
-         OR Patients.ID IS NOT NULL);
+FROM (
+     -- Берём уникальность
+     SELECT ActiveInsurance.ID
+     FROM ActiveInsurance
+              LEFT JOIN Doctors ON ActiveInsurance.ID = Doctors.PersonalDataID
+              LEFT JOIN Patients ON ActiveInsurance.ID = Patients.PersonalDataID
+     WHERE Doctors.ID IS NOT NULL
+        OR Patients.ID IS NOT NULL);
