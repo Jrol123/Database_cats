4061587d6b637d7e1244989b8f63ca3fbce007d3
Artemii <angap4@gmail.com>
2023-12-28 10:42:56 +1000

    04G AW

--- 04/04G.sql
+++ 04/04G.sql
@@ -1,67 +1,19 @@
-SELECT date        AS date,
-       count_true  AS number_of_new,
-       count_false AS number_of_completed
-FROM (SELECT MAX(ID)                               AS ID,
-             task_id,
-             substr(timestamp, 0, 10 + 1)          as date,
---        COUNt(status_type) as total_count,
-             COUNT(IIF(status_type == 1, 1, NULL)) as count_true,
-             COUNT(IIF(status_type == 2, 1, NULL)) as count_false
-      FROM incident_table
-      WHERE status_type in (1, 2)
-      GROUP BY substr(timestamp, 0, 10 + 1)
-      ORDER BY timestamp) as max_id_filter
-GROUP BY date
-ORDER BY date;
-
-SELECT MAX(ID)                               AS ID,
-       task_id,
-       substr(timestamp, 0, 10 + 1)          as date,
-       COUNT(status_type)                    as total_count,
-       COUNT(IIF(status_type == 1, 1, NULL)) as count_true,
-       COUNT(IIF(status_type == 2, 1, NULL)) as count_false
-FROM incident_table
-WHERE status_type in (1, 2)
-GROUP BY substr(timestamp, 0, 10 + 1)
-ORDER BY substr(timestamp, 0, 10 + 1);
-
-SELECT MAX(ID)         AS ID,
-       task_id,
-       DATE(timestamp) as date
---        COUNT(IIF(status_type == 1, 1, NULL)) AS number_of_new,
---        COUNT(IIF(status_type == 2, 1, NULL)) AS number_of_completed
-FROM incident_table
--- WHERE status_type in (1, 2)
---   AND DATE(timestamp) >= (SELECT DATE(MAX(DATE(timestamp)), '-30 day') FROM incident_table)
-GROUP BY ID, date
-ORDER BY date;
-
--- 1. Смотришь на запись
--- 2. Фильтруешь заявки, которые были до неё на -30 дней
--- 3. Выбираешь последние действия с заявками за весь промежуток
--- 4. Фильтруешь заявки по статусу (1 или 2).
--- 5. Считаешь
-
-SELECT MAX(ID)         AS ID,
-       task_id,
-       DATE(timestamp) as date
-FROM incident_table
-         JOIN (SELECT MAX(ID)         AS sub_ID,
-                      task_id         as sub_task_id,
-                      DATE(timestamp) as sub_date
-               FROM incident_table
-               GROUP BY ID
-               ORDER BY sub_date) AS full_table
-WHERE DATE('2017-12-29', '-30 days') <= full_table.sub_date
-AND DATE('2017-12-29') >= full_table.sub_date
-GROUP BY ID
-ORDER BY date;
-
-SELECT 2017 - 12 - 02 <= DATE(DATE(timestamp), '-30 day') as date
-FROM incident_table
-GROUP BY ID
-ORDER BY date;
-
-UPDATE incident_table
-SET timestamp = '2023-11-25 18:34:40'
-WHERE id = 7;
+SELECT date,
+       SUM(new_statuses) OVER (ORDER BY date)       AS number_of_new,
+       SUM(completed_statuses) OVER (ORDER BY date) AS number_of_completed
+FROM (SELECT STRFTIME('%Y-%m-%d', timestamp)             AS date,
+             CASE WHEN last_status = 1 THEN 1 ELSE 0 END AS new_statuses,
+             CASE WHEN last_status = 2 THEN 1 ELSE 0 END AS completed_statuses
+      FROM (SELECT id as ID,
+                   LAST_VALUE(status_type) OVER (
+                       PARTITION BY STRFTIME('%d', timestamp), task_id
+                       ORDER BY timestamp
+                       RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+                       )
+                      AS last_status,
+                   timestamp,
+                   task_id
+            FROM incident_table)
+      GROUP BY timestamp, task_id
+      ORDER BY timestamp)
+GROUP BY date;
